bbcbb315a819473ab4d9896471ce6d150495b332
berg <towong@qpp.com>
2023-10-23 10:55:19 +0800

    feat-init: 初始化项目

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @description:
+ * @author: TT-Berg
+ * @date: ${DATE}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
