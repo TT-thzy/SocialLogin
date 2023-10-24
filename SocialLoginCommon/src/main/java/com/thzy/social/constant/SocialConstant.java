a20e7f93e3d0bae47acd2981506128697f71e4d6
berg <towong@qpp.com>
2023-10-24 14:13:29 +0800

    feat-core: 移除拦截器

--- SocialLoginCommon/src/main/java/com/thzy/social/constant/SocialConstant.java
+++ SocialLoginCommon/src/main/java/com/thzy/social/constant/SocialConstant.java
@@ -8,9 +8,5 @@ package com.thzy.social.constant;
  **/
 public interface SocialConstant {
 
-    String cpgInitFileLibraryUrl = "cgp-initFileLibraryUrl";
-
-    String cgpSendRegisterSuccessEmail = "cgp-sendRegisterSuccessEmail";
-
     String THIRD_AUTH_STATE_CACHE_EXPIRE_TIME = "thirdAuthStateCacheExpireTime";
 }
